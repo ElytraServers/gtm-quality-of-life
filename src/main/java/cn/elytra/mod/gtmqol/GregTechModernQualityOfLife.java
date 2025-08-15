2a620b86014e23a2185565de063fd4224e97d3a6
Taskeren <r0yalist@outlook.com>
2025-08-16 07:10:23 +0800

    Clean up

--- src/main/java/cn/elytra/mod/gtmqol/GregTechModernQualityOfLife.java
+++ src/main/java/cn/elytra/mod/gtmqol/GregTechModernQualityOfLife.java
@@ -1,32 +1,18 @@
 package cn.elytra.mod.gtmqol;
 
-import net.minecraft.world.item.Item;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Mod(GregTechModernQualityOfLife.MOD_ID)
 public class GregTechModernQualityOfLife {
 
     public static final String MOD_ID = "gtmqol";
 
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_ID);
+
     public GregTechModernQualityOfLife() {
-        FMLJavaModLoadingContext context = FMLJavaModLoadingContext.get();
-
-        IEventBus bus = context.getModEventBus();
-        Registrar.init(bus);
-    }
-
-    public static class Registrar {
-
-        public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
-
-        public static void init(IEventBus bus) {
-            ITEMS.register(bus);
-        }
-
+        LOG.info("Quality!");
     }
 
 }
--- src/main/java/cn/elytra/mod/gtmqol/QualityThings.java
+++
@@ -1,4 +0,0 @@
-package cn.elytra.mod.gtmqol;
-
-public class QualityThings {
-}
